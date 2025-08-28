e346d486c7fecf67d3a1f6bbd2899d698f322484
Teo <matteo@peculiarventures.com>
2025-08-28 21:13:49 +0300

    chore(isp): update file using ISP principle

--- isp-violation.ts
+++ isp-violation.ts
@@ -1,25 +1,55 @@
 // This code violates the Interface Segregation Principle
 // The interface is too large and forces classes to implement methods they don't need
-interface Worker {
+// interface Worker {
+//     work(): void;
+//     eat(): void;
+//     sleep(): void;
+//     code(): void;
+//     test(): void;
+//     deploy(): void;
+//     manageMeetings(): void;
+// }
+
+// // This class is forced to implement methods it doesn't need
+// class QAEngineer implements Worker {
+//     work() { console.log('Working...'); }
+//     eat() { console.log('Eating lunch...'); }
+//     sleep() { console.log('Sleeping...'); }
+    
+//     // These methods are not relevant for QA but must be implemented
+//     code() { throw new Error('QA does not code'); }
+//     deploy() { throw new Error('QA does not deploy'); }
+//     manageMeetings() { throw new Error('QA does not manage meetings'); }
+    
+//     test() { console.log('Testing the application...'); }
+// }
+
+interface BasicWorker {
     work(): void;
     eat(): void;
     sleep(): void;
+}
+
+interface Coding {
     code(): void;
+}
+
+interface Testing {
     test(): void;
+}
+
+interface Deployment {
     deploy(): void;
+}
+
+interface Managing {
     manageMeetings(): void;
 }
 
-// This class is forced to implement methods it doesn't need
-class QAEngineer implements Worker {
+class QAEngineer implements BasicWorker, Testing {
     work() { console.log('Working...'); }
     eat() { console.log('Eating lunch...'); }
     sleep() { console.log('Sleeping...'); }
-    
-    // These methods are not relevant for QA but must be implemented
-    code() { throw new Error('QA does not code'); }
-    deploy() { throw new Error('QA does not deploy'); }
-    manageMeetings() { throw new Error('QA does not manage meetings'); }
     
     test() { console.log('Testing the application...'); }
 }
